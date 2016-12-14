7c85ee1ef73d8361401cd50314a1fbb312ea666a
Alexey Gordeyev <aleksej@gordejev.lv>
2016-12-14 13:23:05 +0200

    RSASign and RSAVerify made compatible with OpenSSL

--- src/jsencrypt.js
+++ src/jsencrypt.js
@@ -10,6 +10,66 @@ ASN1.prototype.getHexStringValue = function () {
     return hexString.substr(offset, length);
 };
 
+// PKCS#1 (type 1) pad input string s to n bytes, and return a bigint
+function pkcs1pad1(s,n) {
+  if(n < s.length + 11) { // TODO: fix for utf-8
+    console.error("Message too long for RSA");
+    return null;
+  }
+  var ba = new Array();
+  var i = s.length - 1;
+  while(i >= 0 && n > 0) {
+    var c = s.charCodeAt(i--);
+    if(c < 128) { // encode using utf-8
+      ba[--n] = c;
+    }
+    else if((c > 127) && (c < 2048)) {
+      ba[--n] = (c & 63) | 128;
+      ba[--n] = (c >> 6) | 192;
+    }
+    else {
+      ba[--n] = (c & 63) | 128;
+      ba[--n] = ((c >> 6) & 63) | 128;
+      ba[--n] = (c >> 12) | 224;
+    }
+  }
+  ba[--n] = 0;
+  while(n > 2) { 
+    ba[--n] = 0xFF;
+  }
+  ba[--n] = 1;
+  ba[--n] = 0;
+  return new BigInteger(ba);
+}
+
+// Undo PKCS#1 (type 1) padding and, if valid, return the plaintext
+function pkcs1unpad1(d,n) {
+  var b = d.toByteArray();
+  var i = 0;
+  while(i < b.length && b[i] == 0) ++i;
+  if(b.length-i != n-1 || b[i] != 1)
+    return null;
+  ++i;
+  while(b[i] != 0)
+    if(++i >= b.length) return null;
+  var ret = "";
+  while(++i < b.length) {
+    var c = b[i] & 255;
+    if(c < 128) { // utf-8 decode
+      ret += String.fromCharCode(c);
+    }
+    else if((c > 191) && (c < 224)) {
+      ret += String.fromCharCode(((c & 31) << 6) | (b[i+1] & 63));
+      ++i;
+    }
+    else {
+      ret += String.fromCharCode(((c & 15) << 12) | ((b[i+1] & 63) << 6) | (b[i+2] & 63));
+      i += 2;
+    }
+  }
+  return ret;
+}
+
 /**
  * Method to parse a pem encoded string containing both a public or private key.
  * The method will translate the pem encoded string in a der encoded string and
@@ -313,7 +373,7 @@ RSAKey.prototype.parsePropertiesFrom = function (obj) {
  */
 RSAKey.prototype.sign = function (text, digestMethod) {
     var digest = digestMethod(text);
-    var m = pkcs1pad2(digest, (this.n.bitLength() + 7) >> 3);
+    var m = pkcs1pad1(digest, (this.n.bitLength() + 7) >> 3);
     if (m == null) return null;
     var c = m.modPow(this.d, this.n);
     if (c == null) return null;
@@ -327,7 +387,7 @@ RSAKey.prototype.verify = function (text, signature, digestMethod) {
     var c = parseBigInt(signature, 16);
     var m = c.modPowInt(this.e, this.n);
     if (m == null) return null;
-    var digest = pkcs1unpad2(m, (this.n.bitLength() + 7) >> 3);
+    var digest = pkcs1unpad1(m, (this.n.bitLength() + 7) >> 3);
     return digest == digestMethod(text);
 };
 /**
